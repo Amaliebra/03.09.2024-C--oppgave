2fbb864e76c100ced6d6229e58694e7974c3340b
amaliebra <jba082024@gmail.com>
2024-09-03 13:25:16 +0200

    initial commit

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Drawing;
+
+namespace _03._09._2024_C__oppgave;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        int myNumber = 8685;
+        string myWords = "cool";
+        double myDecimal = 488.54;
+        char myLetter = 'a';
+        Console.WriteLine(myDecimal);
+        Console.WriteLine(myWords);
+        Console.WriteLine(myNumber);
+        Console.WriteLine(myLetter);
+    }
+}
