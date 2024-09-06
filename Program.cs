457242bc5f83e0ea7a477ba16b9efc827a5e3aa5
amaliebra <jba082024@gmail.com>
2024-09-06 10:05:56 +0200

    added the assignment text

--- Program.cs
+++ Program.cs
@@ -7,25 +7,31 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        //Lag en variabel av datatypen integer og tildel den et vilkårlig heltall
         int myNumber = 8685;
+        //Lag en variabel av datatypen string og tilden en vilkårlig tekst
         string myWords = "cool";
+        //Lag en variabel av datatypen double og tilden et vilkårlig tall, både desimal og heltall er gyldig med denne typen
         double myDecimal = 488.54;
+        //Lag en variabel av datatypen char og tildel den en vilkårlig karakter(bokstav)
         char myLetter = 'a';
         Console.WriteLine(myDecimal);
         Console.WriteLine(myWords);
         Console.WriteLine(myNumber);
         Console.WriteLine(myLetter);
 
+        //Lag et Array av datatypen integer og skriv ut verdiene med en vanlig for-løkke
         int[] theIntArray = { 1, 2, 3, 4, 5 };
         for (int i = 0; i <= 5; i++) ;
 
+        //Lag et Array av datatypen string og skriv ut verdiene med en foreach-løkke
         string[] theStringArray = { "cool", "waffle", "Grim" };
         foreach (string i in theStringArray)
         {
             Console.WriteLine(i);
         }
 
+        //Lag en List av datatypen string og tildel verdier ved hjelp av List sin innebygde metode "Add()"
         List<string> thisList = new List<string>();
         thisList.Add("list element1");
         thisList.Add("list element2");
@@ -33,9 +39,6 @@ class Program
         {
             Console.WriteLine(thisOne);
         }
-
-
-
 
     }
 }
